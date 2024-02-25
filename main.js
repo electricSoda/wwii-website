82d582fe774b9cee4f751122f892fbbc1a355a0d
electricSoda <justin.hua.ge@gmail.com>
2024-02-25 15:36:33 -0700

    Added collapsing slider for section 4

--- main.js
+++ main.js
@@ -55,4 +55,23 @@ $(function () { // wait for document ready
 					stepSize         : 50, // [px]
 	});
 
+    var coll = document.getElementsByClassName("collapsible");
+
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            for (let j = 0; j < coll.length; j++) {
+                if (coll[j]===this) continue;
+                coll[j].classList.remove("active");
+                coll[j].nextElementSibling.style.maxHeight = null;
+            }
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } 
+    });
+}
+
 });
